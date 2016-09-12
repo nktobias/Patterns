a55a8098b7ff1c0527d827a484b99d0c6a4ac5f5
Ivan Janovic <ivan.janovic>
2016-09-12 08:11:37 +0200

    Initial

---
+++ Patterns/DiceRoller/DiceRoller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DiceRoller
+{
+    public class DiceRoller
+    {
+        private Random _random;
+        private static DiceRoller _instance;
+
+        public static DiceRoller Instance => _instance ?? (_instance = new DiceRoller());
+
+        private DiceRoller()
+        {
+            _random = new Random();
+        }
+
+        public int RollSix()
+        {
+            return _random.Next(1, 7);
+        }
+
+    }
+}
---
+++ Patterns/DiceRoller/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DiceRoller.Instance.RollSix());
+            Console.WriteLine(DiceRoller.Instance.RollSix());
+            Console.WriteLine(DiceRoller.Instance.RollSix());
+            Console.WriteLine(DiceRoller.Instance.RollSix());
+            Console.WriteLine(DiceRoller.Instance.RollSix());
+
+            Console.ReadLine();
+        }
+    }
+}
